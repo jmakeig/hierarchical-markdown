9883b0d9a4f2e61ae687326312ca5c8182bb3655
Justin Makeig <jmakeig@marklogic.com>
2017-10-22 15:58:35 -0700

    Adds client-side table of contents (#4)
    The default Pandoc ToC wasn’t quite usable. The numbering assumes the top-level headings start at h1, not h2. (The h1 is supposed to come from the document `title` metadata.)
    
        pandoc \
          --from=markdown_strict+header_attributes+yaml_metadata_block\
          --to=html5 \
          --self-contained \
          --template="hierarchical/template.html" \
          --section-divs \
          --output="$1.html" \
          --toc \
          --toc-depth=6 \
          "$1.md"

--- templates/hierarchical/behavior.js
+++ templates/hierarchical/behavior.js
@@ -10,6 +10,7 @@ for (const section of sections) {
 document.body.addEventListener('click', evt => {
   if (evt.target && evt.target.matches('.toggleable')) {
     evt.target.classList.toggle('toggle-collapsed');
+    evt.stopPropagation();
   }
 });
 
@@ -43,3 +44,74 @@ const lastBuilt = document.querySelector('.last-built');
 if (lastBuilt) {
   lastBuilt.textContent = now;
 }
+
+function* getHeading(node) {
+  const children = node.querySelector('h1, h2, h3, h4, h5, h6, .pseudo-heading')
+    .childNodes;
+
+  // Nodes are by reference, not value
+  // So, you need to clone first
+  for (const child of children) yield child.cloneNode();
+}
+
+function getTOC(root = document.body) {
+  function traverse(node, accumulator) {
+    if (!node) return accumulator;
+    while (node) {
+      accumulator.push({
+        // Iterable of Node instances, so you can use HTML in your headings
+        heading: getHeading(node),
+        href: node.id ? `#${node.id}` : undefined,
+        children: traverse(walker.firstChild(), []),
+      });
+      // The currrentNode is updated on recursive traversal.
+      // Need to reset to traverse siblings.
+      walker.currentNode = node;
+      node = walker.nextSibling();
+    }
+    return accumulator;
+  }
+  const walker = document.createTreeWalker(
+    root,
+    NodeFilter.SHOW_ELEMENT,
+    {
+      acceptNode: node => {
+        if ('SECTION' === node.nodeName) {
+          return NodeFilter.FILTER_ACCEPT;
+        }
+        return NodeFilter.FILTER_REJECT;
+      },
+    },
+    false
+  );
+
+  return traverse(walker.nextNode(), [], walker);
+}
+
+function renderTOC(toc, levels = [0]) {
+  const ul = document.createElement('ul');
+  for (const item of toc) {
+    const a = document.createElement('a');
+    a.setAttribute('href', item.href);
+    // Increment the leaf level
+    levels[levels.length - 1]++;
+    const level = `${levels.join('.')}. `;
+    const span = document.createElement('span');
+    span.appendChild(document.createTextNode(level));
+    a.appendChild(span);
+    // item.heading is a NodeList, not a string
+    for (const titleChild of item.heading) {
+      a.appendChild(titleChild);
+    }
+    const li = document.createElement('li');
+    li.appendChild(a);
+    if (item.children && item.children.length > 0) {
+      li.classList.add('toggleable');
+      li.appendChild(renderTOC(item.children, [...levels, 0]));
+    }
+    ul.appendChild(li);
+  }
+  return ul;
+}
+
+document.querySelector('#TOC').appendChild(renderTOC(getTOC()));
